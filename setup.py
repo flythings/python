d12738be327b1d327aa1ff3cec7f76e20eb124fa
ITGNET\cbarros <cbarros@itg.es>
2017-06-01 14:07:54 +0200

    version 0.6

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'flythings',
   packages = ['flythings'], # this must be the same as the name above
-  version = '0.5',
+  version = '0.6',
   description = 'A python library to add observations into flythings',
   author = 'flythings',
   author_email = 'gblazquez@itg.es',
   license='MIT',
   url = 'https://github.com/flythings/python', # use the URL to the github repo
-  download_url = 'https://github.com/flythings/python/0.5.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/flythings/python/0.6.tar.gz', # I'll explain this in a second
   keywords = ['flythings'], # arbitrary keywords
   classifiers = [],
   install_requires=[
