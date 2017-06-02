1bdcedf2eea6e67dfb6ff0152fca807a1faac383
ITGNET\cbarros <cbarros@itg.es>
2017-06-02 12:07:09 +0200

    Adding some test.

---
+++ flythingsTest/flythingsTest.py
@@ -0,0 +1,23 @@
+import flythings
+import time
+
+
+#Server, user and password are specified in the configuration File, if you dont have it, create the Configuration.properties file and specify those fields.
+def sendObservation_test():
+	x = flythings.sendObservation(20,"op","uoms",None,None,"procedure","foi")
+	assert str(x) == str(b'{"message":"Full insertion","type":"Ok"}')
+
+def sendObservations_test():
+	observations = []
+	observations.append(flythings.getObservation(20, "test1", None, None, None, "ob1", "multiple"))
+	observations.append(flythings.getObservation(30, "test2", None, None, None, "ob1", "multiple"))
+	observations.append(flythings.getObservation(40, "test3", None, None, None, "ob1", "multiple"))
+	observations.append(flythings.getObservation(50, "test4", "uoms", round(time.time() * 1000), None, "ob1", "multiple"))
+	x = flythings.sendObservations(observations)
+	assert str(x) == str('{"message":"Full insertion","type":"Ok"}')
+
+
+def search_test():
+	x = flythings.search(947, 1495643746000, 1496248546000)
+	assert str(x[1]['time']) == "1496218547000"
+	assert str(x[1]['value']) == "20.0"
