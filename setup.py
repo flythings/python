d985469247b42cfa2bdc5c417b2dd64b3c3acc1c
Carlos Barros <cbarros@itg.es>
2018-03-13 16:39:45 +0100

    update pip configuration

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'flythings',
   packages = ['flythings'],
-  version = '0.9.0',
+  version = '0.9.1',
   description = 'A python library to add observations into flythings',
   author = 'flythings',
   author_email = 'gblazquez@itg.es',
   license='GPL-3.0',
   url = 'https://github.com/flythings/python',
-  download_url = 'https://github.com/flythings/python/0.9.0.tar.gz',
+  download_url = 'https://github.com/flythings/python/0.9.1.tar.gz',
   keywords = ['flythings'],
   classifiers = [],
   install_requires=[
