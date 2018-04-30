0314676abdfa826ffb30d2147df27304160fd254
Carlos Barros <cbarros@itg.es>
2018-04-30 15:50:45 +0200

    version 0.9.3

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'flythings',
   packages = ['flythings'],
-  version = '0.9.2',
+  version = '0.9.3',
   description = 'A python library to add ans search observations into flythings',
   author = 'flythings',
   author_email = 'tic@itg.es',
   license='GPL-3.0',
   url = 'https://github.com/flythings/python',
-  download_url = 'https://github.com/flythings/python/0.9.2.tar.gz',
+  download_url = 'https://github.com/flythings/python/0.9.3.tar.gz',
   keywords = ['flythings', 'IoT'],
   classifiers = [],
   install_requires=[
